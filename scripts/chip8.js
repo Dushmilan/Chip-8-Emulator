dbba856ad7f9e598ef7b5c89cd154f4316c27ebd
shaash23 <shahanakugan5@gmail.com>
2026-01-17 00:41:06 +0530

    hlo

--- scripts/chip8.js
+++ scripts/chip8.js
@@ -4,3 +4,27 @@ const renderer  = new Renderer(10);
 let loop;
 
 let fps = 60 , fpsInterval, startTime, now, then, elapsed;
+
+function init() {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+
+    //TESTING CODE. REMOVE WHEN DONE TESTING
+    renderer.testRender();
+    renderer.render();
+    //END TESTING CODE
+    
+    loop = requestAnimationFrame(step);
+}
+
+function step() {
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        // Cycle the CPU. We'll come back to this later and fill
+    }
+    loop = requestAnimationFrame(step);
+}
+init()
